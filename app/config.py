4939760cb7080b5704f2edc19b9e8f4b86c352c6
Marcelo Teixeira dos Santos <marcelo.teixeira@cern.ch>
2021-10-31 16:09:05 +0100

    Configure deploy to Heroku

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
+from os import environ
+
+if environ.get('DATABASE_URL'):
+    SQLALCHEMY_DATABASE_URI = (
+        environ.get('DATABASE_URL').replace("://", "ql://", 1))
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///storage/database.db'
+
 DEBUG = True
 CSRF_ENABLED = False
 SECRET_KEY = 'codecademyisajoke'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///storage/database.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
